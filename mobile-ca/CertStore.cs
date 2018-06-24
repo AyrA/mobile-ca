1a9ad45a6d62160338b95f1bb18ac892769bcfa2
Kevin Gut <kevin.gut@bitm.sg>
2018-06-25 00:10:47 +0200

    Cleanup

--- mobile-ca/CertStore.cs
+++ mobile-ca/CertStore.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Text.RegularExpressions;
 
 namespace mobile_ca
 {
--- mobile-ca/Server.cs
+++ mobile-ca/Server.cs
@@ -5,7 +5,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 
